19c65b333dcf03acd11de8c7e24ac5575cdd667e
Alina <aaetbaeva@yandex.ru>
2022-05-13 16:15:02 +0500

    Create news

--- test/skills/news/views.py
+++ test/skills/news/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import News
+
 
 def index(request):
-    # print(request)
-    return HttpResponse('Hello world')
+    news = News.objects.order_by('-created_at')
+    content = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    return render(request, 'news/index.html', content)
 
 
-def test(request):
-    # print(request)
-    return HttpResponse('<h1>Test page</h1>')
