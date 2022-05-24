ffd3b74b7498fc9f36dd597ce0f779ce1e800cc7
Alina <aaetbaeva@yandex.ru>
2022-05-24 14:15:15 +0500

    With custom tag 21 lesson

--- test/skills/news/models.py
+++ test/skills/news/models.py
@@ -8,7 +8,8 @@ class News(models.Model):
     update_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True, verbose_name='Фото')
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
+
     def __str__(self):
         return self.title
 
@@ -20,6 +21,9 @@ class News(models.Model):
 
 class Category(models.Model):
     title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование')
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         verbose_name = 'Категория'
--- test/skills/news/urls.py
+++ test/skills/news/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from news.views import *
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='home'),
+    path('category/<int:category_id>/', get_category, name='category'),
 ]
